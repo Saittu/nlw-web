5e8952107485d81c954a5b5de79b243f1fad935b
Jean <jeanants@icloud.com>
2024-09-15 21:08:48 -0300

    end NLW, linking back to front

--- src/app.tsx
+++ src/app.tsx
@@ -2,12 +2,22 @@ import { Dialog } from './assets/componets/ui/dialog'
 import { CreateGoal } from './assets/componets/create-goal'
 import { EmptyGoals } from './assets/componets/empty-goals'
 import { Summary } from './assets/componets/summary'
+import { useQuery } from '@tanstack/react-query'
+import { getSummary } from './http/get-summary'
+
+
 
 export function App() {
+  const { data } = useQuery({
+  queryKey: ['summary'],
+  queryFn: getSummary,
+})
+  
+
   return (
     <Dialog>
-      {/* <EmptyGoals /> */}
-      <Summary />
+      {data?.total && data.total > 0 ? <Summary /> : <EmptyGoals />}
+
       <CreateGoal />
     </Dialog>
   )
